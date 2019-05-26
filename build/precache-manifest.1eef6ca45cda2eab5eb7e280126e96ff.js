2164b90598fef04eae5ffd459f336dc60b838a8a
Silvia Rebelo <silvi04@gmail.com>
2019-05-26 13:43:53 +0100

    Pushing build files

---
+++ build/precache-manifest.1eef6ca45cda2eab5eb7e280126e96ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb4093339565d38cfc6a61ab85d2f9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94ec926504bd1412db20",
+    "url": "/static/css/main.c0c48f19.chunk.css"
+  },
+  {
+    "revision": "3fe8b424ed1bb02e6ae9",
+    "url": "/static/js/2.235c107c.chunk.js"
+  },
+  {
+    "revision": "94ec926504bd1412db20",
+    "url": "/static/js/main.918155ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
