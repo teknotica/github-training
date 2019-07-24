a95254e596f7d3b0303db576ee6df2a820775e0d
Silvia Rebelo <silvi04@gmail.com>
2019-07-24 12:40:26 +0100

    Adding button to page for demo

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,57 @@
 import React from "react";
+import { PrimaryButton } from "../components/button";
 
 const Home = () => (
   <div className="page">
     <h1>Quokka</h1>
     <p>
-        <img src="https://tinyurl.com/yy7htq8h" alt="" />
+      <img src="https://tinyurl.com/yy7htq8h" alt="" />
     </p>
     <h2>Description</h2>
     <p>
-        The quokka weighs 2.5 to 5 kilograms (5.5 to 11.0 lb) and is 40 to 54 centimetres (16 to 21 in) long with a 25-to-30-centimetre-long (9.8 to 11.8 in) tail, which is quite short for a macropod. It has a stocky build, well developed hind legs, rounded ears, and a short, broad head. Its musculoskeletal system was originally adapted for terrestrial bipedal saltation but over its evolution its system has been built for arboreal locomotion.[7] Although looking rather like a very small kangaroo, it can climb small trees and shrubs up to 1.5 metres (4 ft 11 in).[8] Its coarse fur is a grizzled brown colour, fading to buff underneath. The quokka is known to live for an average of ten years.[9]
+      The quokka weighs 2.5 to 5 kilograms (5.5 to 11.0 lb) and is 40 to 54
+      centimetres (16 to 21 in) long with a 25-to-30-centimetre-long (9.8 to
+      11.8 in) tail, which is quite short for a macropod. It has a stocky build,
+      well developed hind legs, rounded ears, and a short, broad head. Its
+      musculoskeletal system was originally adapted for terrestrial bipedal
+      saltation but over its evolution its system has been built for arboreal
+      locomotion.[7] Although looking rather like a very small kangaroo, it can
+      climb small trees and shrubs up to 1.5 metres (4 ft 11 in).[8] Its coarse
+      fur is a grizzled brown colour, fading to buff underneath. The quokka is
+      known to live for an average of ten years.[9]
     </p>
     <p>
-        Quokkas have a promiscuous mating system.[10] After a month of gestation, females give birth to a single baby called a joey. Females can give birth twice a year and produce approximately seventeen joeys during their lifespan.[9] The joey lives in its mother’s pouch for six months. Once it leaves the pouch, the joey relies on its mother for milk for two more months and is fully weaned approximately eight months after birth.[9] Females sexually mature after roughly 18 months.[11] When a female quokka with a joey in her pouch is pursued by a predator, she may drop her baby onto the ground; the joey produces noises, which may serve to attract the predator’s attention while the mother escapes.[12]
+      Quokkas have a promiscuous mating system.[10] After a month of gestation,
+      females give birth to a single baby called a joey. Females can give birth
+      twice a year and produce approximately seventeen joeys during their
+      lifespan.[9] The joey lives in its mother’s pouch for six months. Once it
+      leaves the pouch, the joey relies on its mother for milk for two more
+      months and is fully weaned approximately eight months after birth.[9]
+      Females sexually mature after roughly 18 months.[11] When a female quokka
+      with a joey in her pouch is pursued by a predator, she may drop her baby
+      onto the ground; the joey produces noises, which may serve to attract the
+      predator’s attention while the mother escapes.[12]
+    </p>
+    <p>
+      <PrimaryButton label="Read more" />
     </p>
     <h2>Ecology</h2>
     <p>
-        <img src="https://tinyurl.com/y3dcs269" alt="" />
+      <img src="https://tinyurl.com/y3dcs269" alt="" />
     </p>
     <p>
-        In the wild, its roaming is restricted to a very small range in the South-West of Western Australia, with a number of small scattered populations. There is one large population on Rottnest Island and a smaller population on Bald Island near Albany. The islands are free of certain predators such as foxes and cats. On Rottnest, quokkas are common and occupy a variety of habitats ranging from semi-arid scrub to cultivated gardens.[16] Prickly Acanthocarpus plants, which are unaccommodating for humans and other relatively large animals to walk through, provide their favorite daytime shelter for sleeping.[17] Additionally, they are known for their ability to climb trees.[9]
+      In the wild, its roaming is restricted to a very small range in the
+      South-West of Western Australia, with a number of small scattered
+      populations. There is one large population on Rottnest Island and a
+      smaller population on Bald Island near Albany. The islands are free of
+      certain predators such as foxes and cats. On Rottnest, quokkas are common
+      and occupy a variety of habitats ranging from semi-arid scrub to
+      cultivated gardens.[16] Prickly Acanthocarpus plants, which are
+      unaccommodating for humans and other relatively large animals to walk
+      through, provide their favorite daytime shelter for sleeping.[17]
+      Additionally, they are known for their ability to climb trees.[9]
     </p>
-</div>
+  </div>
 );
 
 export default Home;
